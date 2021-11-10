cc94ea0a8e9f330546c6fee7723758a348d46641
wulanfrom <biasindi.wulanfrom@gmail.com>
2021-11-10 15:39:07 +0900

    style fixes and trend graph init

--- src/components/Trend.js
+++ src/components/Trend.js
@@ -16,26 +16,42 @@ import data from "../dummyData/data";
 flatMap.shim();
 
 function Trend() {
-    const [data, setData] = useState([]);
-    const d3ref = React.useRef(null);
-    const dataRef = React.useRef(flatMap(data, (e) => e));
+    const [data] = useState([25, 50, 35, 15, 94, 10]);
+    const svgRef = useRef(); // for the svg container
+
 
     useEffect(() => {
-        regenerateData();
+        // setting up the svg
+        const w = 800;
+        const h = 300;
+        const svg = d3.select(svgRef.current).attr('width', w).attr('height', h).style('background','#d3d3d3').style('margin', '50').style('overflow', 'visible');
+        // setting up the scaling
+        const xScale = d3.scaleLinear().domain([0, data.length - 1]).range([0, w])
+        const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]); //range starts from top to bottom
+        const generateScaledLine = d3.line().x((d, i) => xScale(i)).y(yScale).curve(d3.curveCardinal); // plotting the lines using the scales we created
+
+        // setting the axes
+        const xAxis = d3.axisBottom(xScale).ticks(data.length).tickFormat(i => i + 1);
+        const yAxis = d3.axisLeft(yScale).ticks(5);
+        svg.append('g').call(xAxis).attr('transform', `translate(0, ${h})`);
+        svg.append('g').call(yAxis);
+
+        // setting up the data for the svg
+        svg.selectAll('.line').data([data]).join('path').attr('d', (d) => generateScaledLine(d)).attr('fill', 'none').attr('stroke', 'black');
     }, [])
 
-    function regenerateData() {
-        const chartData = [];
-        for (let i = 0; i < 20; i++) {
-            const value = Math.floor(Math.random() * i + 3);
-            chartData.push({
-                label: i.toFixed,
-                value,
-                tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
-            });
-        }
-        setData(chartData)
-    }
+    // function regenerateData() {
+    //     const chartData = [];
+    //     for (let i = 0; i < 20; i++) {
+    //         const value = Math.floor(Math.random() * i + 3);
+    //         chartData.push({
+    //             label: i.toFixed,
+    //             value,
+    //             tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
+    //         });
+    //     }
+    //     setData(chartData)
+    // }
 
     return (
         <div>
@@ -49,9 +65,7 @@ function Trend() {
                     <Button>Social Health</Button>
                 </ButtonGroup>
                 <Box sx={{ my: "1.5rem" }} style={{border: '0px solid red', height: '93%'}}>
-                    <Button onClick={regenerateData}>Change Data</Button>
-                    {/* <div ref={d3ref}></div> */}
-                    <TrendChart data={data} width={400} height={300}/> 
+                    <svg ref={svgRef}></svg>
                 </Box>
             </div>
             <div className="panels">
