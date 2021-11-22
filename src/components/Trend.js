72a65a858405cf96afaabbcbb0ab826770f58712
jeongeonp <jeongeonpark1@gmail.com>
2021-11-22 17:54:05 +0900

    Add small multiples

--- src/components/Trend.js
+++ src/components/Trend.js
@@ -4,6 +4,9 @@ import { Button } from 'semantic-ui-react'
 
 import './Pages.css';
 
+import physical from '../assets/physical_agg.json';
+import mental from '../assets/mental_agg.json';
+
 import mental_p3012 from '../assets/mental-p3012.json';
 import mental_others from '../assets/mental-others.json';
 
@@ -13,10 +16,8 @@ import createPlotlyComponent from "react-plotly.js/factory";
 const Plot = createPlotlyComponent(Plotly);
 
 
-console.log(mental_others)
-console.log(Object.values(mental_others.total_mean))
+console.log(physical)
 
-const times = Object.keys(mental_p3012.total_mean)
 var total = {}
 
 var mental_total = mental_p3012
@@ -40,15 +41,65 @@ var dates = {
 function Trend() {
     const [myData, setMyData] = useState(dates)
     const [othersData, setOthersData] = useState(dates)
-    
+    const [multipleMyData, setMultipleMyData] = useState(null)
+    const [multipleOthersData, setMultipleOthersData] = useState(null)
+
+    const [selectedAspect, setSelectedAspect] = useState('mental')
+
+    // move to hook later
+    const [numMetric, setNumMetric] = useState({physical: ['Calories', 'Pedometer'], mental: ['Valence', 'Arousal', 'Attention', 'Stress'], social: ['Calllog', 'Messagelog', 'SNSlog']})
+
+    useEffect(() => {
+        console.log('selectedAspect changed')
+        changeAspect(selectedAspect)
+    }, [selectedAspect])
+
     const handleClickAspect = (e) => {
+        setSelectedAspect(e)
+    }
+
+    const changeAspect = (e) => {
+        if (e === 'physical') {
+            const newPhysical = {x: dates.x, y: Object.values(physical.z_physical)}
+            const newOthersPhysical = {x: dates.x, y: [0, 0, 0, 0, 0, 0, 0]}
+            const newMultiplePhysical = {}
+            const newOthersMultiplePhysical = {}
+            for (var i in numMetric[selectedAspect]) {
+                newMultiplePhysical[numMetric['physical'][i]] = {x: dates.x, y: Object.values(physical['p3012_'+numMetric['physical'][i]])}
+                newOthersMultiplePhysical[numMetric['physical'][i]] = {x: dates.x, y: Object.values(physical['others_'+numMetric['physical'][i]])}
+            }
+            setMyData(newPhysical)
+            setOthersData(newOthersPhysical)
+            setMultipleMyData(newMultiplePhysical)
+            setMultipleOthersData(newOthersMultiplePhysical)
+        }
+        
         if (e === 'mental') {
-            const newMental = {x: dates.x, y: Object.values(mental_total.total_mean)}
-            const newOthersMental = {x: dates.x, y: Object.values(mental_others.total_mean)}
+            const newMental = {x: dates.x, y: Object.values(mental.z_mental)}
+            const newOthersMental = {x: dates.x, y: [0, 0, 0, 0, 0, 0, 0]}
+            const newMultipleMental = {}
+            const newOthersMultipleMental = {}
+            for (var i in numMetric[selectedAspect]) {
+                newMultipleMental[numMetric['mental'][i]] = {x: dates.x, y: Object.values(mental['p3012_'+numMetric['mental'][i]])}
+                newOthersMultipleMental[numMetric['mental'][i]] = {x: dates.x, y: Object.values(mental['others_'+numMetric['mental'][i]])}
+            }
+            
+            //console.log(newMultipleMental)
+            //console.log(newOthersMultipleMental)
+
             setMyData(newMental)
             setOthersData(newOthersMental)
+            setMultipleMyData(newMultipleMental)
+            setMultipleOthersData(newOthersMultipleMental)
+
+            numMetric[selectedAspect].map((e) => {console.log(newMultipleMental[e].x)})
+        }
+
+        if (e === 'social') {
+
         }
     }
+    
 
     return (
         <div>
@@ -81,22 +132,42 @@ function Trend() {
                             name: "Other Users' Average",
                         },
                         ]}
-                        layout={{width: 1000, height: 600}}
+                        layout={{width: 1000, height: 600, title: selectedAspect.charAt(0).toUpperCase()+selectedAspect.slice(1)+" Health", yaxis: {range: [-4, 4]}}}
                     />
-                </Box>
-                <Box sx={{ my: "1.5rem" }} style={{border: '0px solid red', height: '93%'}}>
                 </Box>
             </div>
             <div className="panels">
                 <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
-                    <h4 className="panel-title">Detailed trends for each metric</h4>
-                    {/*<ButtonGroup sx={{ m: 2 }} variant="outlined" aria-label="outlined primary button group">
-                        <Button startIcon={<ViewAgendaIcon />}>Vertical View</Button>
-                        <Button startIcon={<ViewColumnIcon/>}>Horizontal View</Button>
-                    </ButtonGroup>*/}
+                    <h4 className="panel-title">Detailed trends for each metric</h4> 
                 </Box>
                 <div style={{border: '0px solid red', height: '93%'}}>
-
+                    {multipleMyData !== null && multipleOthersData !== null
+                    ? numMetric[selectedAspect].map((e) => (
+                        <Plot
+                            data={[
+                            {
+                                x: multipleMyData[e].x,
+                                y: multipleMyData[e].y,
+                                type: 'scatter',
+                                mode: 'lines+markers',
+                                marker: {color: '#F88923'},
+                                name: 'You',
+                            },
+                            {
+                                x: multipleOthersData[e].x,
+                                y: multipleOthersData[e].y,
+                                type: 'scatter',
+                                mode: 'lines+markers',
+                                marker: {color: 'gray'},
+                                name: "Other Users",
+                            },
+                            ]}
+                            layout={{width: 500, height: 300, title: e}}
+                        />
+                    ))
+                    : null
+                    }
+                    
                 </div>
             </div>
         </div>
