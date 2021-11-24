4209a052badc1536776d6ba912dcf27ed6675a2f
sangkyung <skkwak9806@kaist.ac.kr>
2021-11-24 14:47:52 +0900

    activity data added

--- src/components/Activities-lib.js
+++ src/components/Activities-lib.js
@@ -1,52 +1,78 @@
 import React, { useRef, useState, useEffect} from 'react';
-import { select } from "d3";
-import * as d3 from 'd3';
-import { ResponsiveCirclePackingCanvas } from '@nivo/circle-packing'
-
+import { ResponsiveCirclePackingCanvas } from '@nivo/circle-packing';
 
 import './Pages.css';
 
-function Activities() {
-    const goodSvgRef = useRef(null);
-    const badSvgRef = useRef(null);
-    const [good_data, setGoodData] = useState([
-        {source:"UV Exposure", x: 120, y: 130, val: 100000, color: "#01C696"},
-        {source:"Calories", x: 130, y: 330, val: 30000, color: "#01C696"},
-        {source:"Attention", x: 320, y: 130, val: 40000, color: "#01C696"},
-        {source:"Call Log", x: 310, y: 300, val: 80000, color: "#01C696"},
-      ]);
-    const [bad_data, setBadData] = useState([
-        {source:"Message Entity", x: 70, y: 60, val: 35000, color: "#FF8974"},
-        {source:"SNS App Usage", x: 50, y: 180, val: 10000, color: "#FF8974"},
-        {source:"Stress Level", x: 190, y: 200, val: 50000, color: "#FF8974"},
-    ])
+import activity from '../assets/overall_cmp.json';
 
-    const data = {
+console.log(activity)
+
+function Activities() {
+    const [numElement, setNumElement] = useState(6)
+
+    const good_data = {
         "name": "root",
         "children": [
             {
-            "name": "node.0",
-            "value": 84
+            "name": "Message Log",
+            "value": 0.7961778933,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
+
             },
             {
-            "name": "node.1",
-            "value": 67
+            "name": "Arousal",
+            "value": 0.4626475787,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
+            },       
+        ]
+    }
+
+    const bad_data = {
+        "name": "root",
+        "children": [
+            {
+            "name": "Valence",
+            "value": 0.009320999554,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
             },
             {
-            "name": "node.2",
-            "value": 28
+            "name": "Pedometer",
+            "value": 0.03814630631,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
             },
             {
-            "name": "node.3",
-            "value": 22
+            "name": "Sns Ratio",
+            "value": 0.09563653303,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
             },
             {
-            "name": "node.4",
-            "value": 2
+            "name": "Call Log",
+            "value": 0.1202137128,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
             },
             {
-            "name": "node.5",
-            "value": 38
+            "name": "Calories",
+            "value": 0.1395309915,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
+            },
+            {
+            "name": "Attention",
+            "value": 0.3183039581,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
+            },
+            {
+            "name": "Stress",
+            "value": 0.5556021315,
+            "p3012": 0.7253143682,
+            "others": 0.7317367553,
             },
             
         ]
@@ -66,16 +92,34 @@ function Activities() {
                 <div className="controlBar"></div>
                 <div className="goodBubble">
                     <ResponsiveCirclePackingCanvas
-                        data={data}
+                        data={good_data}
                         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
                         id="name"
                         colors="#01C696"
-                        colorBy="id"
-                        childColor={{ from: 'color', modifiers: [ [ 'brighter', 0.4 ] ] }}
-                        padding={10}
+                        padding={45}
                         leavesOnly={true}
                         enableLabels={true}
-                        label="value"
+                        label="id"
+                        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 2.4 ] ] }}
+                        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.3 ] ] }}
+                        animate={true}
+                        tooltip={({id, value, p3012, others}) => (
+                            <div style={{ color:'black', backgroundColor: 'white', padding: '0px 5px', boxShadow: '1px 1px 1px gray', borderRadius: '1px' }}>
+                                {p3012}: <b>{others}</b>
+                            </div>
+                        )}
+                    />
+                </div>
+                <div className="badBubble">
+                    <ResponsiveCirclePackingCanvas
+                        data={bad_data}
+                        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                        id="name"
+                        colors="#ff8974"
+                        padding={45}
+                        leavesOnly={true}
+                        enableLabels={true}
+                        label="id"
                         labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 2.4 ] ] }}
                         borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.3 ] ] }}
                         animate={true}
@@ -85,8 +129,6 @@ function Activities() {
                             </div>
                         )}
                     />
-                </div>
-                <div className="badBubble">
                     
                 </div>
             </div>
@@ -98,9 +140,11 @@ function Activities() {
                 <span className="description">The categories are determined by</span>
                 <div className="circleBlock">
                     <div className="goodCircle"></div>
+                    <p>Good Activity</p>
                 </div>
                 <div className="circleBlock">
                     <div className="badCircle"></div>
+                    <p>Bad Activity</p>
                 </div>
             </div>
         </div>
